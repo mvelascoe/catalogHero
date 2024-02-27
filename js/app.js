48c6e5b2a72fbb267c679080d1a24b12bf72ce61
mvelascoe <maritza.94v@gmail.com>
2024-02-27 13:48:14 -0500

    feat: :sparkles: se realiza la ventana emergente
    Se realiza la ventana Emergente

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@ function createHeroCards(heroData) {
   <div class="card-content">
     <h3>${heroData.name}</h3>
     <p>${heroData.description}</p>
-    <button id="abrirModal">Ver más</button>
+    <button id="${heroData.name}" class="boton">Ver más</button>
   </div>`;
 
   //<a href="" class="btn-more">Ver más</a>
+// const btn = querySelector('#abrirModal');
 
   const customDiv = document.createElement("div"); // Elemento padre
     customDiv.className = "card";
@@ -24,26 +25,51 @@ data.forEach(function (heroData) {
     createHeroCards(heroData);
 });
 
+const carta = document.querySelector("section.cardsmarvel")
 
+carta.addEventListener("click", (event) =>{
+  if(event.target.classList.contains("boton")){
+    const personaje = event.target.id
+    mostrarModal(personaje)
+  }
+});
 
-const modal = document.getElementById("ventanaModal");
-
-const boton = document.getElementById("abrirModal");
+const boton = document.querySelector(".boton");
+// const modal = document.querySelector(".modal")
 
 const span = document.getElementsByClassName("cerrar")[0];
 
-boton.addEventListener("click",function() {
-  modal.style.display = "block";
-});
+const modal = document.getElementById("ventanaModal");
+
+function mostrarModal(heroe){
+    const datos = buscarHero(heroe)
+    console.log(datos)
+    modal.style.display = "block";
+    const h2 = document.querySelector(".tituloModal")
+    h2.textContent = datos.name
+
+    const imgs = document.querySelector(".imagen")
+    imgs.src = datos.img
+
+    const parrafo = document.querySelector(".descripcion")
+    parrafo.textContent = datos.description
+
+
+  }
+
+  function buscarHero(nombre){
+    const heroe = data.find((h) => h.name === nombre)
+    return heroe
+  }
 
 span.addEventListener("click",function() {
   modal.style.display = "none";
 });
 
-window.addEventListener("click",function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-});
+// window.addEventListener("click",function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// });
 
 
