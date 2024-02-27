c6598904d8b462297c406b28726bed3ff205c5bf
marivelascoe <maritza.94v@gmail.com>
2024-02-27 09:03:15 -0500

    feat: :sparkles: se agrega el resto de imagenes
    Se agregan el resto de imagenes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 import data from '../data.json' assert {type: 'json'};
 
 function createHeroCards(heroData) {
-  const cardInnerHTML = `<div class="card-img">
-  <img src="${heroData.img}" alt="Personaje1">
-</div>
-<div class="card-content">
-  <h3>${heroData.name}</h3>
-  <p>${heroData.description}</p>
-  <a href="" class="btn-more">Ver más</a>
-</div>`;
+  const cardInnerHTML = `
+  <div class="card-img">
+    <img src="${heroData.img}" alt="Personaje">
+  </div>
+  <div class="card-content">
+    <h3>${heroData.name}</h3>
+    <p>${heroData.description}</p>
+    <button id="abrirModal">Ver más</button>
+  </div>`;
+
+  //<a href="" class="btn-more">Ver más</a>
 
   const customDiv = document.createElement("div"); // Elemento padre
     customDiv.className = "card";
@@ -20,3 +23,27 @@ function createHeroCards(heroData) {
 data.forEach(function (heroData) {
     createHeroCards(heroData);
 });
+
+
+
+const modal = document.getElementById("ventanaModal");
+
+const boton = document.getElementById("abrirModal");
+
+const span = document.getElementsByClassName("cerrar")[0];
+
+boton.addEventListener("click",function() {
+  modal.style.display = "block";
+});
+
+span.addEventListener("click",function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click",function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+
