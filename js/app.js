2f9324601eb7848dda77b284f1ce230fa261d136
marivelasco <maritza.94v@gmail.com>
2024-02-27 23:08:49 -0500

    feat: :construction: se agrega estructura
    Se agrega estructura HTML

--- js/app.js
+++ js/app.js
@@ -1,75 +1,77 @@
-import data from '../data.json' assert {type: 'json'};
+import data from "../data.json" assert { type: "json" };
 
-function createHeroCards(heroData) {
+console.log(data)
+
+function createHeroCards(heroData, sectionSelector) {
   const cardInnerHTML = `
-  <div class="card-img">
-    <img src="${heroData.img}" alt="Personaje">
-  </div>
-  <div class="card-content">
-    <h3>${heroData.name}</h3>
-    <p>${heroData.description}</p>
-    <button id="${heroData.name}" class="boton">Ver más</button>
-  </div>`;
+    <div class="card-img">
+      <img src="${heroData.img}" alt="Personaje">
+    </div>
+    <div class="card-content">
+      <h3>${heroData.name}</h3>
+      <p>${heroData.description}</p>
+      <button id="${heroData.name}" class="boton">Ver más</button>
+    </div>`;
 
-  //<a href="" class="btn-more">Ver más</a>
-// const btn = querySelector('#abrirModal');
-
-  const customDiv = document.createElement("div"); // Elemento padre
-    customDiv.className = "card";
+  const customDiv = document.createElement("div");
+  customDiv.className = "card";
   customDiv.innerHTML = cardInnerHTML;
 
-  document.querySelector("section.cardsmarvel").appendChild(customDiv);
+  document.querySelector(sectionSelector).appendChild(customDiv);
 }
 
-data.forEach(function (heroData) {
-    createHeroCards(heroData);
+data.marvel.forEach(function (heroData) {
+  createHeroCards(heroData, "section.cardsmarvel");
 });
 
-const carta = document.querySelector("section.cardsmarvel")
-
-carta.addEventListener("click", (event) =>{
-  if(event.target.classList.contains("boton")){
-    const personaje = event.target.id
-    mostrarModal(personaje)
-  }
+data.dc.forEach(function (heroData) {
+  createHeroCards(heroData, "section.cardsDC");
 });
+
+//Ventana Emergente
 
 const boton = document.querySelector(".boton");
-// const modal = document.querySelector(".modal")
-
 const span = document.getElementsByClassName("cerrar")[0];
-
 const modal = document.getElementById("ventanaModal");
 
-function mostrarModal(heroe){
-    const datos = buscarHero(heroe)
-    console.log(datos)
-    modal.style.display = "block";
-    const h2 = document.querySelector(".tituloModal")
-    h2.textContent = datos.name
+const cartaMarvel = document.querySelector("section.cardsmarvel");
+const cartaDC = document.querySelector("section.cardsDC");
 
-    const imgs = document.querySelector(".imagen")
-    imgs.src = datos.img
-
-    const parrafo = document.querySelector(".descripcion")
-    parrafo.textContent = datos.description
-
-
+cartaMarvel.addEventListener("click", (event) => {
+  if (event.target.classList.contains("boton")) {
+    const personaje = event.target.id;
+    mostrarModal(personaje);
   }
-
-  function buscarHero(nombre){
-    const heroe = data.find((h) => h.name === nombre)
-    return heroe
-  }
-
-span.addEventListener("click",function() {
-  modal.style.display = "none";
 });
 
-// window.addEventListener("click",function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// });
+cartaDC.addEventListener("click", (event) => {
+  if (event.target.classList.contains("boton")) {
+    const personaje = event.target.id;
+    mostrarModal(personaje);
+  }
+});
 
+function mostrarModal(heroe) {
+  const datos = buscarHero(heroe);
+  // console.log(datos);
+  modal.style.display = "block";
+  const h2 = document.querySelector(".tituloModal");
+  h2.textContent = datos.name;
 
+  const imgs = document.querySelector(".imagen");
+  imgs.src = datos.img;
+
+  const parrafo = document.querySelector(".descripcion");
+  parrafo.textContent = datos.description;
+}
+
+function buscarHero(nombre) {
+  const heroeMarvel = data.marvel.find((h) => h.name === nombre);
+  const heroeDC = data.dc.find((h) => h.name === nombre);
+
+  return heroeMarvel || heroeDC;
+}
+
+span.addEventListener("click", function () {
+  modal.style.display = "none";
+});
